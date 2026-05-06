cf3b3a077c49562ad92d7e80e5180e49b3fcec01
Muzamil Hassan <muzamilhassan247@gmail.com>
2026-05-06 17:05:10 +0500

    chora & feat build file and folder set frontend

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,13 +1,5 @@
-"use client";
-
-import { useEffect, useState } from "react"
+import { redirect } from "next/navigation";
 
 export default function Home() {
-    const [data, setData] = useState("");
-    useEffect(() => {
-    fetch("http://localhost:5000/health")
-      .then(res => res.text())
-      .then(data => setData(data));
-  }, []);
-  return <div>{data}</div>;
+    redirect('/auth/signIn');
 }
