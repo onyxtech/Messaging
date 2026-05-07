b48b2a2bc825522fbdc59ede7970979083b7f992
Muzamil Hassan <muzamilhassan247@gmail.com>
2026-05-07 14:26:18 +0500

    feat: db connection

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,14 +1,11 @@
 import app from './app';
 import 'dotenv/config';
-
-
-
-
+import { connectDB } from './config/db';
 
 const PORT = process.env.PORT || 3000;
 // Start server
 const startServer = async () => {
-  
+    await connectDB();
 //   app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 
     app.listen(PORT, () => {
